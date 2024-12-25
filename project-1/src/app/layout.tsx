6fb386f03782a02fb81d101e7059383ece4320ea
user24122morke <user24122morke@protonmail.com>
2024-12-25 18:13:40 +0000

    change favicon

--- project-1/src/app/layout.tsx
+++ project-1/src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/favicon.png" />
+        <link rel="icon" href="/logo.png" />
       </head>
       <body className={inter.className}>
        
