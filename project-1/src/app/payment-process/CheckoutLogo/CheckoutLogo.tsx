48067e4dc9699d5cf31c7ea0c71fba22c0c4eab9
user24122morke <user24122morke@protonmail.com>
2025-02-19 10:36:49 +0000

    change adress for chackout and logo

--- project-1/src/app/payment-process/CheckoutLogo/CheckoutLogo.tsx
+++ project-1/src/app/payment-process/CheckoutLogo/CheckoutLogo.tsx
@@ -15,7 +15,7 @@ const CheckoutLogo: React.FC = () => {
         className=" w-[30rem] ml-8 -mb-24 -mt-14" // Spațiu între logo și text
       /> */}
       {/* Textul Globalixa */}
-      <h3 className=" text-4xl font-bold text-gray-800">Globalixa</h3>
+      <h3 className=" text-4xl font-bold text-gray-800">Payment Process</h3>
     </div>
   );
 };
