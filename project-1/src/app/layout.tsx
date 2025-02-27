13f9744078a1a16fa731a86f35907da5e5dd9037
user24122morke <user24122morke@protonmail.com>
2025-02-27 13:13:17 +0100

    removed icon for logo

--- project-1/src/app/layout.tsx
+++ project-1/src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/logo.png" />
+{/*         <link rel="icon" href="/logo.png" /> */}
       </head>
       <body className={inter.className}>
        
